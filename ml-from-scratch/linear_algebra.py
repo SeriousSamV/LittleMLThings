8568f39b4a689bebcb46f8f78a992cb896290d03
Samuel Vishesh Paul <paulsamuelvishesh@live.com>
2025-01-05 20:33:42 +0530

    Add basic matrix operations to linear algebra module
    Introduced functions for matrix shape, row/column retrieval, creation, and identity matrix generation. These enhancements expand the module's capabilities for handling matrices, enabling more complex linear algebra operations. Included tests ensure correctness of the implemented functions.

--- ml-from-scratch/linear_algebra.py
+++ ml-from-scratch/linear_algebra.py
@@ -83,3 +83,46 @@ def distance(v: Vector, w: Vector) -> float:
 
 
 assert distance([5, 7], [2, 11]) == 5.0
+
+Matrix = List[Vector]
+
+
+def shape(A: Matrix) -> tuple[int, int]:
+    num_rows = len(A)
+    num_cols = len(A[0]) if A else 0
+    return num_rows, num_cols
+
+
+assert shape([[1, 2, 3], [4, 5, 6]]) == (2, 3)
+
+
+def get_row(A: Matrix, i: int) -> Vector:
+    return A[i]
+
+
+assert get_row([[1, 2, 3], [4, 5, 6]], 0) == [1, 2, 3]
+
+
+def get_column(A: Matrix, j: int) -> Vector:
+    return [A_i[j] for A_i in A]
+
+
+assert get_column([[1, 2, 3], [4, 5, 6]], 0) == [1, 4]
+
+
+def make_matrix(num_rows: int, num_cols: int, entry_fn: callable) -> Matrix:
+    return [[entry_fn(i, j) for i in range(num_cols)] for j in range(num_rows)]
+
+
+assert make_matrix(2, 3, lambda i, j: i + j) == [[0, 1, 2], [1, 2, 3]]
+
+
+def identity_matrix(n: int) -> Matrix:
+    return make_matrix(n, n, lambda i, j: 1 if i == j else 0)
+
+
+assert identity_matrix(5) == [[1, 0, 0, 0, 0],
+                              [0, 1, 0, 0, 0],
+                              [0, 0, 1, 0, 0],
+                              [0, 0, 0, 1, 0],
+                              [0, 0, 0, 0, 1]]
